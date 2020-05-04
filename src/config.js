2387460013c65c3bcc077396d2a3ceea4fa1c094
Melanie Hallowell <melleehall@gmail.com>
2020-05-04 16:40:55 -0600

    Modified API endpoint to reflect path update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `https://pure-dusk-67487.herokuapp.com/contacts`,
+  API_ENDPOINT: `https://pure-dusk-67487.herokuapp.com/api/contacts`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
