e7607162b60361c61d516f1dcfb9243a3b3150ed
Melanie Hallowell <melleehall@gmail.com>
2020-05-04 20:09:05 -0600

    Removed development default for API BASE URL to see if live app connects to hosted server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api/contacts",
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
