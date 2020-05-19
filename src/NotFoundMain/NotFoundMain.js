affd7a1646f84f73cc72e695ccbcc28ebd8f95d7
Melanie Hallowell <melleehall@gmail.com>
2020-05-18 21:27:02 -0600

    Added heading to NotFoundMain per WAVE recommendation

--- src/NotFoundMain/NotFoundMain.js
+++ src/NotFoundMain/NotFoundMain.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function RenderNotFoundMain() {
     return(
-        <p>We are sorry, but the page you are trying to reach does not exist.  If you are looking for information on Clear Creek Broadband, please visit <a href='https://www.clearcreekbroadband.com'>www.clearcreekbroadband.com</a>.</p>
+        <div>
+            <h1>Page Not Found</h1>
+            <p>We are sorry, but the page you are trying to reach does not exist.  If you are looking for information on Clear Creek Broadband, please visit <a href='https://www.clearcreekbroadband.com'>www.clearcreekbroadband.com</a>.</p>
+        </div>
     )
 }
