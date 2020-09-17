3d9e68470445e461494e9355ba7b73e534560094
Melanie Hallowell <melleehall@gmail.com>
2020-09-17 13:31:07 -0600

    Network Status Update 9-17

--- src/HomeView/HomeView.js
+++ src/HomeView/HomeView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import bandwmnt from '../Images/bandwmnt.jpg'
 import mntlaptop from '../Images/mntlaptop.jpg'
 import './HomeView.css'
@@ -18,8 +18,7 @@ export default class HomeView extends Component {
                     <section className='intro-p'>
                         <h2 className='latest'>Latest News</h2>
                         <p>
-                            We have primary connection near completion at our main hub site, King Murphy!
-                            We will be testing the equipment and service inside of "pilot" homes by the end of August.
+                            Pictures of the subscriber home equipment are now available here: <NavLink to='/support'>FAQ Page</NavLink>.  
                         </p>
                         <p className='volunteer-p'>
                             If we do not already have your current contact information, please, click on the 'Sign Me Up' button below and submit the form so that we can notify you when service is available for your home.
@@ -30,31 +29,26 @@ export default class HomeView extends Component {
                     <section>
                     <div className='lightestgray net-stat-header'>
                         <h2>Network Status</h2>
-                        <h3>As of: 08-10-2020</h3>
+                        <h3>As of: 09-17-2020</h3>
                     </div>
                     <p className='lightestgray summary'>
-                       We are sure that many of you are wondering what we are up to so here is a quick update...
+                        Our pilot tests were postponed due to delays experienced by another provider who we are reliant on to finish work at our main hub site before we can offer service to our first coverage area.
+                        We expect to be able to start testing in "pilot homes" by the end of next week.
+                        An update will be posted here when testing begins, and the test period will last about three weeks.
+                        We know that this news is not ideal, and hope you understand that some delays are out of our hands due to the current challenges of the world and the providers who we rely on needing to operate safely.
                     </p>
                     <p className='lightestgray summary'>
-                        We have primary connection near completion at King Murphy which is our main hub site.  
-                        We also have all of the internet connections provisioned and will first be testing the radios and then the equipment inside of some “pilot” homes by the end of August.  
-                        The subscribers' home equipment will include a receiver (think of a small satellite dish on the roof or side of the house) which will be wired to a router inside the home.  
-                        We will post some pictures of the equipment on our website to give you an idea of what it looks like.
-                    </p>
-                    <p className='lightestgray summary'>
-                        The other serving locations will be towers which are currently being engineered with permitting and construction to follow.  
-                        We found a local general contractor who has deep Clear Creek roots and has dealt with the county process many times, so we are hopeful that we can get primary construction phases completed before the weather turns bad.  
+                        The pilot phase delay does not impact our timeline for engineering and permitting of the other serving sites which are all towers. 
+                        We are working with a local general contractor who has deep Clear Creek roots and has dealt with the county process many times, so we are hopeful that we can get primary construction phases completed before the weather turns bad.  
                         Once the towers are built, it will take a few weeks to get transmitting radios placed onto the towers and into service.
                         We are still targeting to provide service to all areas by the end of the year. 
                     </p>
                     <p className='lightestgray summary'>
-                        Meanwhile, we are working behind the scenes on adding customer features to the website and procuring equipment so as areas open for service, we are ready to support all subscribers.  
-                        Preparations include developing training and installation processes, customer materials, and other basic tasks needed to run a service.
+                        Pictures of the subscriber home equipment are now available on our FAQ page found here: <NavLink to='/support'>FAQ Page</NavLink>.  
+                        The home equipment includes a receiver (think of a small satellite dish on the roof or side of the house) that is wired to a router inside the home.     
                     </p>
                     <p className='lightestgray summary'>
-                        Thank you all for your support (we got a significant number of prepaid deposits!) and for your interest in our service.  
-                        I know that many of you are eager to have access to high-speed internet and may feel like it is taking a long time, but we were officially awarded funds in March so for all practical purposes, we are a few months into the construction phase.  
-                        There is so much to do and so little time to get it all done, but we will continue to post major updates about the network build and service here.  
+                        Thank you all for your continued support and interest in our service.  Please, continue to share your contact information with us through the Signup page if we do not already have it so that we can get in touch with you when the time comes to schedule your home equipment installation.
                     </p>
                     <div className='img-container'>
                         <img src={mntlaptop} alt='laptop with mountain background' className='img'/>
