07258aa61347df5fece348b692993399bff52fd6
Melanie Hallowell <melleehall@gmail.com>
2020-09-19 15:07:16 -0600

    Network status update 9 17 20

--- src/HomeView/HomeView.js
+++ src/HomeView/HomeView.js
@@ -18,10 +18,10 @@ export default class HomeView extends Component {
                     <section className='intro-p'>
                         <h2 className='latest'>Latest News</h2>
                         <p>
-                            Pictures of the subscriber home equipment are now available here: <NavLink to='/support'>FAQ Page</NavLink>.  
+                            Our first pilot testing site is live!  
                         </p>
                         <p className='volunteer-p'>
-                            If we do not already have your current contact information, please, click on the 'Sign Me Up' button below and submit the form so that we can notify you when service is available for your home.
+                            For the next few weeks, we will be perfecting our installation process, gathering valuable feedback from neighbors participating in the pilot program, and preparing for our commercial service launch for homes covered in our initial service area.
                         </p>
                         <Link to='/signup' id='signup-desktop' className='signup-btn' role='button' aria-pressed='false'>Sign Me Up</Link>                 
                     </section>  
@@ -29,26 +29,27 @@ export default class HomeView extends Component {
                     <section>
                     <div className='lightestgray net-stat-header'>
                         <h2>Network Status</h2>
-                        <h3>As of: 09-17-2020</h3>
+                        <h3>As of: 09-19-2020</h3>
                     </div>
                     <p className='lightestgray summary'>
-                        Our pilot tests were postponed due to delays experienced by another provider who we are reliant on to finish work at our main hub site before we can offer service to our first coverage area.
-                        We expect to be able to start testing in "pilot homes" by the end of next week.
-                        An update will be posted here when testing begins, and the test period will last about three weeks.
-                        We know that this news is not ideal, and hope you understand that some delays are out of our hands due to the current challenges of the world and the providers who we rely on needing to operate safely.
+                        Pilot testing is now underway!  
+                        We are perfecting our installation process, gathering valuable feedback from neighbors participating in the pilot program, and preparing for our commercial service launch for homes covered in our initial service area.
+                        This is an important milestone in our journey and one that we are excited to share with you.
+                    </p>
+
+                    <p className='lightestgray summary'>
+                        Pictures of the subscriber home equipment are now available on our FAQ page found here: <NavLink to='/support'>FAQ Page</NavLink>.  
+                        The home equipment includes a receiver (think of a small satellite dish on the roof or side of the house) that is wired to a router inside the home.     
+                        When we are nearing the end of our pilot test phase, we will begin contacting homeowners that are eligible for service under our initial coverage area to schedule installation appointments.
                     </p>
                     <p className='lightestgray summary'>
-                        The pilot phase delay does not impact our timeline for engineering and permitting of the other serving sites which are all towers. 
+                        Our other serving sites which are all towers are in the engineering and permitting phase.
                         We are working with a local general contractor who has deep Clear Creek roots and has dealt with the county process many times, so we are hopeful that we can get primary construction phases completed before the weather turns bad.  
                         Once the towers are built, it will take a few weeks to get transmitting radios placed onto the towers and into service.
                         We are still targeting to provide service to all areas by the end of the year. 
                     </p>
                     <p className='lightestgray summary'>
-                        Pictures of the subscriber home equipment are now available on our FAQ page found here: <NavLink to='/support'>FAQ Page</NavLink>.  
-                        The home equipment includes a receiver (think of a small satellite dish on the roof or side of the house) that is wired to a router inside the home.     
-                    </p>
-                    <p className='lightestgray summary'>
-                        Thank you all for your continued support and interest in our service.  Please, continue to share your contact information with us through the Signup page if we do not already have it so that we can get in touch with you when the time comes to schedule your home equipment installation.
+                        Thank you all for your continued support and interest in our service.  We will continue to work hard towards bringing coverage to all of our service areas and share important milestones here on the website.
                     </p>
                     <div className='img-container'>
                         <img src={mntlaptop} alt='laptop with mountain background' className='img'/>
