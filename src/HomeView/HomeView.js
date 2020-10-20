458dd9022027699f33f0e7a724a601f83c7d58e4
Melanie Hallowell <melleehall@gmail.com>
2020-10-20 13:25:28 -0600

    Network Status and Latest News Updates 10-20-20

--- src/HomeView/HomeView.js
+++ src/HomeView/HomeView.js
@@ -17,11 +17,12 @@ export default class HomeView extends Component {
                     <div className='wires-container block'>
                     <section className='intro-p'>
                         <h2 className='latest'>Latest News</h2>
-                        <p>
-                            Our first pilot testing site is live!  
+                        <p> 
+                            We will begin offering service to some homes served by the King Murphy hub site by the end of October.  We will soon begin contacting eligible neighbors to schedule site surveys and home equipment installations.
                         </p>
                         <p className='volunteer-p'>
-                            For the next few weeks, we will be perfecting our installation process, gathering valuable feedback from neighbors participating in the pilot program, and preparing for our commercial service launch for homes covered in our initial service area.
+                            Various strains on our projected timeline for serving all areas have required us to revise our target service date for homes not served directly by the hub site.
+                            Please, read the Network Status Update for details.
                         </p>
                         <Link to='/signup' id='signup-desktop' className='signup-btn' role='button' aria-pressed='false'>Sign Me Up</Link>                 
                     </section>  
@@ -29,27 +30,30 @@ export default class HomeView extends Component {
                     <section>
                     <div className='lightestgray net-stat-header'>
                         <h2>Network Status</h2>
-                        <h3>As of: 09-19-2020</h3>
+                        <h3>As of: 10-20-2020</h3>
                     </div>
                     <p className='lightestgray summary'>
-                        Pilot testing is now underway!  
-                        We are perfecting our installation process, gathering valuable feedback from neighbors participating in the pilot program, and preparing for our commercial service launch for homes covered in our initial service area.
-                        This is an important milestone in our journey and one that we are excited to share with you.
-                    </p>
-
-                    <p className='lightestgray summary'>
-                        Pictures of the subscriber home equipment are now available on our FAQ page found here: <NavLink to='/support'>FAQ Page</NavLink>.  
-                        The home equipment includes a receiver (think of a small satellite dish on the roof or side of the house) that is wired to a router inside the home.     
-                        When we are nearing the end of our pilot test phase, we will begin contacting homeowners that are eligible for service under our initial coverage area to schedule installation appointments.
+                        Dear Neighbors, 
+                        We have experienced various strains on our projected timeline and must share with you that our original estimation of service for all homes by the end of the year was too optimistic.  
+                        We do still expect to be able to complete construction of the three primary tower sites at the end of the year with service to follow within 30 to 60 days depending on household location.
                     </p>
                     <p className='lightestgray summary'>
-                        Our other serving sites which are all towers are in the engineering and permitting phase.
-                        We are working with a local general contractor who has deep Clear Creek roots and has dealt with the county process many times, so we are hopeful that we can get primary construction phases completed before the weather turns bad.  
-                        Once the towers are built, it will take a few weeks to get transmitting radios placed onto the towers and into service.
-                        We are still targeting to provide service to all areas by the end of the year. 
+                        Through pilot testing, we have collected data and identified areas needing improvement in our network design.  We are using this information to recalibrate and improve our models.  
+                        Although we are using a common wireless network design and proven technology, our radios are operating on a new frequency.  
+                        This frequency is ideal for our network, however, there are some learning curves in understanding its capabilities.
+                        To supplement model predictions, we will continue to do true boots-on-the-ground scouting to validate our data points and improve our network plan.
                     </p>
                     <p className='lightestgray summary'>
-                        Thank you all for your continued support and interest in our service.  We will continue to work hard towards bringing coverage to all of our service areas and share important milestones here on the website.
+                        We are seeing very good speeds in our pilot testing from the hub site at King Murphy.  
+                        We will begin offering service to some homes in that area by the end of the month and will soon begin contacting eligible neighbors to schedule site surveys and home equipment installations.
+                    </p>
+                    <p className='lightestgray summary'>
+                        Our updated model is helping us to identify areas that will not receive a strong signal directly from a tower and will therefore require repeater sites.                      
+                        Repeater sites do not require any permitting or county processes - only approval from the homeowner of the repeater site.  
+                    </p>
+                    <p className='lightestgray summary'>
+                        We will continue to work through the unique challenges presented by COVID-19 and building a network in the mountains.  
+                        We do recognize the urgency to bring high-speed internet to all homes as quickly as possible and appreciate your continued support.                        
                     </p>
                     <div className='img-container'>
                         <img src={mntlaptop} alt='laptop with mountain background' className='img'/>
