11a873c8b603202faf43d2f21e3d79ffc9d30fde
Melanie Hallowell <melleehall@gmail.com>
2020-11-19 22:07:46 -0700

    Removed outdated FAQ about volunteer responsibilities

--- src/FAQSupportView/FAQSupportView.js
+++ src/FAQSupportView/FAQSupportView.js
@@ -133,14 +133,6 @@ export default class FAQSupportView extends Component {
                             </p>
                             <img src={initialCoverage} alt='Map showing First Phase of Broadband Coverage Area' className='km-map-img'/>
                         </li>
-                        <li>
-                            <p className='faq-question'>What sort of tasks are your volunteers helping with?</p>
-                            <p className='faq-answer'>
-                                Volunteers are primarily focused on contacting each and every neighbor to ensure that they are informed about CCB's service and timeline.  
-                                Additionally, volunteers ensure that we have accurate contact information for each household.  
-                                We are currently looking for volunteers from the Upper Witter Gulch and Echo Hills neighborhoods.
-                            </p>
-                        </li>
                     </ul>
                 </section>
                 <section className='desktop-row-imgs'>
