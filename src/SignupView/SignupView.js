99111db9da87c03d4c5e56ddb71bef6db4014a47
Melanie Hallowell <melleehall@gmail.com>
2021-03-12 08:29:52 -0700

    Replaced Signup form with directions to email to request service

--- src/SignupView/SignupView.js
+++ src/SignupView/SignupView.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import AddContact from '../AddContact/AddContact'
-import SignupPathText from '../SignupPathText/SignupPathText'
+// import AddContact from '../AddContact/AddContact'
+// import SignupPathText from '../SignupPathText/SignupPathText'
 import '../universalstyles.css'
 
 export default class SignupView extends Component {
@@ -8,8 +8,17 @@ export default class SignupView extends Component {
         return (
             <div>
                 <h1 className='black-font'>Hi, Clear Creek Resident! Ready for faster internet?</h1>
-                <AddContact />
-                <SignupPathText />
+                {/* <AddContact /> */}
+                {/* <SignupPathText /> */}
+                <div className='lightestgray'>
+                    <p>
+                        Email us at <a href='mailto:questions@clearcreekbroadband.com?subject=Request for Service'>"questions@clearcreekbroadband.com"</a> with your name, address, email, and phone number to let us know that you would like service when available to your home.  
+                        You will be added to our installation queue.
+                    </p>
+                    <p>
+                        <em>New Signup form coming soon...</em>
+                    </p>
+                </div>
             </div>
         )
     }
